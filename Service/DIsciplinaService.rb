40133561d5bcc49f276a33dc1915318c96ba2a7a
WallacePimentel <wallace.r.p.jr@gmail.com>
2026-01-18 16:15:29 -0300

    feat: adding DisciplinaService to store class Disciplina

---
+++ Service/DIsciplinaService.rb
@@ -0,0 +1,17 @@
+class DisciplinaService
+    def initialize
+        @repositorio_disciplinas = []
+    end
+
+    def adicionar_disciplina(disciplina)
+        @repositorio_disciplinas.push(disciplina)
+    end
+
+    def buscar_disciplina(cod_disciplina)
+        @repositorio_disciplinas.find { |disciplina| disciplina.cod_disciplina == cod_disciplina }
+    end
+
+    def remover_disciplina(disciplina)
+        @repositorio_disciplinas.delete(disciplina)
+    end
+end
