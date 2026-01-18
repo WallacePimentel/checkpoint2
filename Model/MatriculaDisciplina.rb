dc01e0ab16a8a11368e0d69b43d802d6b0edca55
WallacePimentel <wallace.r.p.jr@gmail.com>
2026-01-18 15:36:25 -0300

    feat: adding class 'MatriculaDIsciplina' to represent the 1 to n relationship between  classes Aluno e Disciplina

--- Model/MatriculaDisciplina.rb
+++ Model/MatriculaDisciplina.rb
@@ -1,2 +1,10 @@
 class MatriculaDisciplina
-    
+    attr_reader :key
+    attr_accessor :nota
+
+    def initialize(key,nota, ano_semestre)
+        @key = key
+        @nota = nota
+        @ano_semestre = ano_semestre
+    end
+end
