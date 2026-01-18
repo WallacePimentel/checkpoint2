ddbd82fe3ca9776f05d1a1cc39015e555d9b941f
WallacePimentel <wallace.r.p.jr@gmail.com>
2026-01-18 18:15:20 -0300

    feat: adding news methods to calculate and list CR of alumni

--- Service/AlunoService.rb
+++ Service/AlunoService.rb
@@ -28,4 +28,32 @@ class AlunoService
             puts "Aluno: #{aluno.cod_matricula}"
         end
     end
+
+    def calcular_cr(aluno)
+        somatorio_notas = 0.0
+        somatorio_carga_horaria = 0
+
+        aluno.matriculas_disciplina.each do |matricula_disciplina|
+            disciplina = @disciplina_service.buscar_disciplina(matricula_disciplina.key.cod_disciplina)
+            carga_horaria_atual = disciplina.carga_horaria
+
+            somatorio_notas += matricula_disciplina.nota * carga_horaria_atual
+            somatorio_carga_horaria += carga_horaria_atual
+        end
+
+        if somatorio_carga_horaria == 0
+            return 0.0
+        end
+
+        somatorio_notas / somatorio_carga_horaria
+    end
+
+    def listar_cr_dos_alunos
+        puts "------- O CR dos Alunos é -------"
+        @repositorio_alunos.each do |aluno|
+            cr = calcular_cr(aluno)
+            puts "#{aluno.cod_matricula} - #{cr.round(0)}"
+        end
+        puts "----------------------------------"
+    end
 end
