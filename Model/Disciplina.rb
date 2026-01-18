ea926bfa75f965e8f0677aa61fab008c87a9b563
WallacePimentel <wallace.r.p.jr@gmail.com>
2026-01-18 15:13:53 -0300

    feat: adding class 'DIsciplina'

---
+++ Model/Disciplina.rb
@@ -0,0 +1,10 @@
+class Disciplina
+
+    attr_accessor :carga_horaria
+    attr_accessor :curso
+    
+    def initialize(cod_disciplina, curso, carga_horaria)
+        @cod_disciplina = cod_disciplina
+        @curso = curso
+        @carga_horaria = carga_horaria
+    end
