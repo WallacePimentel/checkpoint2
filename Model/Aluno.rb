e848f91c07fde3844a37f4e7a7fa4a84b947c137
WallacePimentel <wallace.r.p.jr@gmail.com>
2026-01-18 15:07:41 -0300

    feat: adding class 'Aluno' and some methods

---
+++ Model/Aluno.rb
@@ -0,0 +1,22 @@
+class Aluno
+
+    def initialize(cod_matricula)
+        @cod_matricula = cod_matricula
+        @disciplinas = []
+    end
+
+    def adicionar_disciplina(disciplina)
+        @disciplinas.push(disciplina)
+    end
+
+    def remover_disciplina(disciplina)
+        @disciplinas.delete(disciplina)
+    end
+
+    def listar_disciplinas
+        @disciplinas.each do |disciplina|
+            puts disciplina
+        end
+    end
+
+    
