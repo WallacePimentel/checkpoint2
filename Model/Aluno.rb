c3f48d66ab9aa1787565d90b86e8654aef954db8
WallacePimentel <wallace.r.p.jr@gmail.com>
2026-01-18 16:00:52 -0300

    fix: fixing 'disciplinas' to 'matriculas_disciplina' and adding getter to array

--- Model/Aluno.rb
+++ Model/Aluno.rb
@@ -1,26 +1,27 @@
 class Aluno
     attr_reader :cod_matricula
+    attr_reader :matriculas_disciplina
 
     def initialize(cod_matricula)
         @cod_matricula = cod_matricula
-        @disciplinas = []
+        @matriculas_disciplina = []
     end
 
-    def adicionar_disciplina(disciplina)
-        @disciplinas.push(disciplina)
+    def adicionar_matriculas_disciplina(matriculas_disciplina)
+        @matriculas_disciplina.push(matriculas_disciplina)
     end
 
-    def remover_disciplina(disciplina)
-        @disciplinas.delete(disciplina)
+    def remover_matriculas_disciplina(matriculas_disciplina)
+        @matriculas_disciplina.delete(matriculas_disciplina)
     end
 
-    def buscar_disciplina(disciplina)
-        @disciplinas.include?(disciplina)
+    def buscar_matriculas_disciplina(matriculas_disciplina)
+        @matriculas_disciplina.include?(matriculas_disciplina)
     end
 
-    def listar_disciplinas
-        @disciplinas.each do |disciplina|
-            puts disciplina
+    def listar_matriculas_disciplina
+        @matriculas_disciplina.each do |matricula_disciplina|
+            puts matricula_disciplina
         end
     end
 end
