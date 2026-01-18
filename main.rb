9925883efd014bab269c798251335ce8e4b91b23
WallacePimentel <wallace.r.p.jr@gmail.com>
2026-01-18 17:03:26 -0300

    feat: adding imports and initializing services

--- main.rb
+++ main.rb
@@ -1,6 +1,23 @@
 require 'csv'
+require_relative 'Model/Aluno'
+require_relative 'Model/Curso'
+require_relative 'Model/Disciplina'
+require_relative 'Model/MatriculaDisciplina'
+require_relative 'Service/AlunoService'
+require_relative 'Service/DisciplinaService'
+require_relative 'Service/CursoService'
+require_relative 'Service/MatriculaDisciplinaService'
+require_relative 'Util/Key'
+
+#Inicializando os serviços como singletons
+aluno_service = AlunoService.instance
+disciplina_service = DisciplinaService.instance
+curso_service = CursoService.instance
+matricula_disciplina_service = MatriculaDisciplinaService.instance
+
+aluno_service.configure(disciplina_service)
+curso_service.configure(aluno_service)
 
 CSV.foreach('notas.csv', headers: true) do |row|
-    puts row['MATRICULA']
-    puts row['NOTA']
+    end
 end
